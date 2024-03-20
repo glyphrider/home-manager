33f745cd9471fade113eb1b6d982a09e9c477c50
Brian H. Ward <glyphrider@gmail.com>
2024-03-20 13:27:51 -0400

    switch to nixpkgs unstable and home-manager master

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "Home Manager configuration of brian";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     hyprland.url = "github:hyprwm/Hyprland";
